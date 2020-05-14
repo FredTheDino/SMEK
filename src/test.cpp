0d30636e948ac8a4ac6b6b761819f6a3ffdf6164
Gustav Sörnäs <gustav@sornas.net>
2020-05-15 00:29:29 +0200

    pretty print with colors, file and line number

--- src/test.cpp
+++ src/test.cpp
@@ -1,16 +1,21 @@
+#include <cstdio>
+
+#include "util/color.h"
+
 #include "test.h"
 
 TestSuite _global_tests = {};
 
-int reg_test(const char *name, TestCallback func) {
+int reg_test(const char *name, TestCallback func, const char *file, unsigned int line) {
     Test t = {};
     t.name = name;
     t.func = func;
+    t.file = file;
+    t.line = line;
     _global_tests.add(t);
     return 0;
 }
 
-#include <stdio.h>
 
 void TestSuite::add(Test test) {
     if (tests == nullptr || cap_tests == num_tests) {
@@ -31,15 +36,25 @@ void TestSuite::add(Test test) {
 }
 
 void TestSuite::run() {
-    GameState state;
-    printf("Running tests\n");
+    std::printf("Running %d tests\n", num_tests);
     unsigned int succeeded = 0;
+
+    GameState state;
     for (unsigned int i = 0; i < num_tests; i++) {
-        bool success = tests[i].func(&state);
-        printf("  %d: %s %d\n", tests[i].id, tests[i].name, success);
-        succeeded += success;
+        std::printf(CLEAR "\r%d/%d:  " YELLOW "testing" RESET " %s\r",
+                i+1, num_tests, tests[i].name);
+        if (tests[i].func(&state)) {
+            succeeded++;
+        } else {
+            std::printf(CLEAR "\r" RED "f" RESET " %s (%s @ %d)\n",
+                    tests[i].name, tests[i].file, tests[i].line);
+        }
     }
-    printf("%d/%d\n", succeeded, num_tests);
+    std::printf(CLEAR);
+    std::printf(GREEN "Passed:" RESET " %d\n", succeeded);
+    if (succeeded != num_tests)
+        std::printf(RED "Failed:" RESET " %d\n", num_tests - succeeded);
+
     delete[] tests;
 }
 
--- src/test.h
+++ src/test.h
@@ -10,15 +10,20 @@
 
 #define UNIQUE_NAME(base) PP_CAT(PP_CAT(base, __LINE__), __COUNTER__)
 
-#define TEST_CASE(name, block) int UNIQUE_NAME(_test_id_) = reg_test((name), [](GameState *game) -> bool block);
+#define _FILE() PP_CAT_II(__FILE__)
+#define _LINE() PP_CAT_II(__LINE__)
+
+#define TEST_CASE(name, block) int UNIQUE_NAME(_test_id_) = reg_test((name), [](GameState *game) -> bool block, _FILE(), _LINE());
 typedef bool(*TestCallback)(GameState *game);
 
-int reg_test(const char *name, TestCallback func);
+int reg_test(const char *name, TestCallback func, const char *file, unsigned int line);
 
 struct Test {
     unsigned int id;
     const char *name;
     TestCallback func;
+    const char *file;
+    unsigned int line;
 };
 
 struct TestSuite {
@@ -33,5 +38,5 @@ struct TestSuite {
 #ifdef TESTS
 extern TestSuite _global_tests;
 #else
-inline int reg_test(const char *, TestCallback) { return 0; }
+inline int reg_test(const char *, TestCallback, const char *, unsigned int) { return 0; }
 #endif
