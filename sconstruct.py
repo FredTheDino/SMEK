6207b9e4af1430608d7095853cd8c5fabb2414d7
Gustav Sörnäs <gustav@sornas.net>
2020-05-15 18:36:35 +0200

    Add run-target and use --tests-flag

--- sconstruct.py
+++ sconstruct.py
@@ -1,16 +1,22 @@
 from glob import glob
 
+AddOption("--tests",
+          dest="tests",
+          action="store_true",
+          help="Compile with tests")
+
 env = Environment()
 env.Replace(CXX="g++")
 
 source = glob("src/**/*.c*", recursive=True)
 
-if ARGUMENTS.get("test", 0):
+if GetOption("tests"):
     env.Append(CPPDEFINES="TESTS")
 else:
     source.remove("src/test.cpp")
 
 smek = env.Program(target="SMEK", source=source)
 Default(smek)
+AlwaysBuild(Alias("run", smek, smek[0].abspath))
 
 env.Clean(smek, glob("src/**/*.o", recursive=True))  # always remove *.o
