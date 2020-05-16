c95ee9181136e067588ed4208d25a3a01079924c
Edvard Thörnros <edvard.thornros@gmail.com>
2020-05-16 12:28:44 +0200

    More work on vectors and such

---
+++ src/math/smek_vec.cpp
@@ -0,0 +1,84 @@
+#include "smek_vec.h"
+#include "../test.h"
+#include <stdio.h>
+
+
+template<typename T>
+constexpr int DIM() {
+    T t; return sizeof(t._) / sizeof(t._[0]);
+}
+
+TEST_STMT("vec_comp_0", DIM<Vec2>() == 2);
+TEST_STMT("vec_comp_1", DIM<Vec3>() == 3);
+TEST_STMT("vec_comp_2", DIM<Vec4>() == 4);
+
+template<typename T>
+T operator +(const T &a, const T &b) {
+    T result;
+    for (u32 i = 0; i < DIM<T>(); i++) result._[i] = a._[i] + b._[i];
+    return result;
+}
+
+TEST_STMT("vec_add_0", close_enough(Vec3(3, 4, 2) + Vec3(1, -2, 2), Vec3(4, 2, 4)));
+TEST_STMT("vec_add_1", close_enough(Vec4() + Vec4(1, 1, 1), Vec4(1, 1, 1)));
+
+template<typename T>
+T operator -(const T &a, const T &b) {
+    T result;
+    for (u32 i = 0; i < DIM<T>(); i++) result._[i] = a._[i] - b._[i];
+    return result;
+}
+
+TEST_STMT("vec_sub_0", close_enough(Vec2(3, 4) - Vec2(1, -2), Vec2(2, 6)));
+TEST_STMT("vec_sub_1", close_enough(Vec4() - Vec4(1, 1, 1), Vec4(-1, -1, -1)));
+
+
+template<typename T>
+T operator *(const T &a, const real &s) {
+    T result;
+    for (u32 i = 0; i < DIM<T>(); i++) result._[i] = a._[i] * s;
+    return result;
+}
+
+TEST_STMT("vec_mul_0", close_enough(Vec2(1, 1) * 3, Vec2(3, 3)));
+
+template<typename T>
+T operator *(const real &s, const T &a) {
+    return a * s;
+}
+
+TEST_STMT("vec_mul_1", close_enough(0 * Vec4(1, 1, 3, 7), Vec4()));
+
+template<typename T>
+T operator /(const T &a, const real &s) {
+    return a * (1.0 / s);
+}
+
+TEST_STMT("vec_div_0", close_enough(Vec4(1, 3, 3, 7) / 1, Vec4(1, 3, 3, 7)));
+TEST_STMT("vec_div_1", close_enough(Vec4(3, 3, 3, 9) / 3, Vec4(1, 1, 1, 3)));
+
+template<typename T>
+T hadamard(const T &a, const T &b) {
+    T result;
+    for (u32 i = 0; i < DIM<T>(); i++) result._[i] = a._[i] * b._[i];
+    return result;
+}
+
+TEST_STMT("vec_hadamard_0", close_enough(hadamard(Vec2(1, 2), Vec2(3, 4)), Vec2(3, 8)));
+
+template<typename T>
+real dot(const T &a, const T &b) {
+    real result;
+    for (u32 i = 0; i < DIM<T>(); i++) result += a._[i] * b._[i];
+    return result;
+}
+
+TEST_STMT("vec_dot_0", close_enough(dot(Vec2(0, 1), Vec2(1, 0)), 0));
+
+template<typename T>
+bool _close_enough_vec(const T &a, const T &b, real r) {
+    bool result = true;
+    for (u32 i = 0; i < DIM<T>(); i++)
+        result &= Math::close_enough(a._[i], b._[i], r);
+    return result;
+}
---
+++ src/math/smek_vec.h
@@ -0,0 +1,77 @@
+#pragma once
+#include "types.h"
+#include "smek_math.h"
+
+// Deliberately placed outside of namespace.
+struct Vec2 {
+    Vec2(real x=0.0, real y=0.0):
+        x(x), y(y) {}
+
+    union {
+        real _[2];
+        struct { real x, y; };
+        struct { real r, g; };
+    };
+};
+
+
+struct Vec3 {
+    Vec3(real x=0.0, real y=0.0, real z=0.0):
+        x(x), y(y), z(z) {}
+
+    union {
+        real _[3];
+        struct { real x, y, z; };
+        struct { real r, g, b; };
+    };
+};
+
+struct Vec4 {
+    Vec4(real x=0.0, real y=0.0, real z=0.0, real w=0.0):
+        x(x), y(y), z(z), w(w) {}
+
+    union {
+        real _[4];
+        struct { real x, y, z, w; };
+        struct { real r, g, b, a; };
+    };
+};
+
+template<typename T>
+constexpr int DIM();
+
+template<typename T>
+T operator +(const T &a, const T &b);
+
+template<typename T>
+T operator -(const T &a, const T &b);
+
+template<typename T>
+T operator *(const T &a, const real &s);
+
+template<typename T>
+T operator *(const real &s, const T &a);
+
+template<typename T>
+T operator /(const T &a, const real &s);
+
+template<typename T>
+T hadamard(const T &a, const T &b);
+
+template<typename T>
+real dot(const T &a, const T &b);
+
+template<typename T>
+bool _close_enough_vec(const T &a, const T &b, real r);
+
+bool close_enough(const Vec2 &a, const Vec2 &b, real r=0.001) {
+    return _close_enough_vec(a, b, r);
+}
+
+bool close_enough(const Vec3 &a, const Vec3 &b, real r=0.001) {
+    return _close_enough_vec(a, b, r);
+}
+
+bool close_enough(const Vec4 &a, const Vec4 &b, real r=0.001) {
+    return _close_enough_vec(a, b, r);
+}
