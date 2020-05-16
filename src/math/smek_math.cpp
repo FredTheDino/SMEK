769d18b2958e88cc7c8092f5d515cadded22c8b8
Edvard Thörnros <edvard.thornros@gmail.com>
2020-05-16 12:43:19 +0200

    More tests for vectors

--- src/math/smek_math.cpp
+++ src/math/smek_math.cpp
@@ -9,36 +9,36 @@ T abs(T a) {
     return a > 0 ? a : -a;
 }
 
-TEST_STMT("abs_0", abs<i32>(-1) == 1);
-TEST_STMT("abs_1", abs<f32>(1.0) == 1.0);
-TEST_STMT("abs_2", abs<i32>(0) == 0);
+TEST_STMT("abs", abs<i32>(-1) == 1);
+TEST_STMT("abs", abs<f32>(1.0) == 1.0);
+TEST_STMT("abs", abs<i32>(0) == 0);
 
 template<typename T>
 T min(T a, T b) {
     return a > b ? b : a;
 }
 
-TEST_STMT("min_0", min<f32>(-1.0, 1.0) == -1.0);
-TEST_STMT("min_1", abs(min<f32>(0.01, 0.02) - 0.01) < 0.001);
-TEST_STMT("min_2", min<i32>(-1.0, -2.0) == -2.0);
+TEST_STMT("min", min<f32>(-1.0, 1.0) == -1.0);
+TEST_STMT("min", abs(min<f32>(0.01, 0.02) - 0.01) < 0.001);
+TEST_STMT("min", min<i32>(-1.0, -2.0) == -2.0);
 
 template<typename T>
 T max(T a, T b) {
     return a < b ? b : a;
 }
 
-TEST_STMT("max_0", max<f32>(-1.0, 1.0) == 1.0);
-TEST_STMT("max_1", abs(max<f32>(0.01, 0.02) - 0.02) < 0.001);
-TEST_STMT("max_2", max<i32>(-1.0, -2.0) == -1.0);
+TEST_STMT("max", max<f32>(-1.0, 1.0) == 1.0);
+TEST_STMT("max", abs(max<f32>(0.01, 0.02) - 0.02) < 0.001);
+TEST_STMT("max", max<i32>(-1.0, -2.0) == -1.0);
 
 template<typename T>
 T sign(T a) {
     return 0 <= a ? 1 : -1;
 }
 
-TEST_STMT("sign_0", sign<f32>(-3.2) == -1.0);
-TEST_STMT("sign_1", sign<f32>(0.02) == 1.0);
-TEST_STMT("sign_2", sign<i32>(0.0) == 1.0);
+TEST_STMT("sign", sign<f32>(-3.2) == -1.0);
+TEST_STMT("sign", sign<f32>(0.02) == 1.0);
+TEST_STMT("sign", sign<i32>(0.0) == 1.0);
 
 template<typename T>
 T sign_with_zero(T a) {
@@ -46,18 +46,18 @@ T sign_with_zero(T a) {
     return sign(a);
 }
 
-TEST_STMT("sign_with_zero_0", sign_with_zero<f32>(-3.2) == -1.0);
-TEST_STMT("sign_with_zero_1", sign_with_zero<f32>(0.02) == 1.0);
-TEST_STMT("sign_with_zero_2", sign_with_zero<i32>(0) == 0);
+TEST_STMT("sign_with_zero", sign_with_zero<f32>(-3.2) == -1.0);
+TEST_STMT("sign_with_zero", sign_with_zero<f32>(0.02) == 1.0);
+TEST_STMT("sign_with_zero", sign_with_zero<i32>(0) == 0);
 
 template<typename T>
 bool close_enough(T a, T b, f32 r) {
     return abs(a - b) < r;
 }
 
-TEST_STMT("close_enough_0", close_enough(1, 1));
-TEST_STMT("close_enough_1", close_enough(1, 2, 3));
-TEST_STMT("close_enough_2", close_enough(0.0, 0.00005));
+TEST_STMT("close_enough", close_enough(1, 1));
+TEST_STMT("close_enough", close_enough(1, 2, 3));
+TEST_STMT("close_enough", close_enough(0.0, 0.00005));
 
 // These are just annoying to write yourself... So I don't bother.
 real pow(real a, real b) { return std::pow(a, b); }
@@ -94,9 +94,9 @@ R cell(real a) {
         return R(a) + 1;
 }
 
-TEST_STMT("cell_0", cell<i32>(0.01f) == 1);
-TEST_STMT("cell_1", cell<i32>(-0.01f) == 0);
-TEST_STMT("cell_2", cell<i32>(20.445f) == 21);
+TEST_STMT("cell", cell<i32>(0.01f) == 1);
+TEST_STMT("cell", cell<i32>(-0.01f) == 0);
+TEST_STMT("cell", cell<i32>(20.445f) == 21);
 
 template<typename R>
 R floor(real a) {
@@ -106,37 +106,37 @@ R floor(real a) {
         return R(a);
 }
 
-TEST_STMT("floor_0", floor<i32>(0.01f) == 0);
-TEST_STMT("floor_1", floor<i32>(-0.01f) == -1);
-TEST_STMT("floor_2", floor<i32>(20.445f) == 20);
+TEST_STMT("floor", floor<i32>(0.01f) == 0);
+TEST_STMT("floor", floor<i32>(-0.01f) == -1);
+TEST_STMT("floor", floor<i32>(20.445f) == 20);
 
 template<typename R>
 R round(real a) {
     return floor<R>(a + 0.5);
 }
 
-TEST_STMT("round_0", round<i32>(0.01f) == 0);
-TEST_STMT("round_1", round<i32>(0.5f) == 1);
-TEST_STMT("round_2", round<i32>(-0.5f) == 0);
-TEST_STMT("round_3", round<i32>(12.231f) == 12);
+TEST_STMT("round", round<i32>(0.01f) == 0);
+TEST_STMT("round", round<i32>(0.5f) == 1);
+TEST_STMT("round", round<i32>(-0.5f) == 0);
+TEST_STMT("round", round<i32>(12.231f) == 12);
 
 real to_radians(real degrees) {
     return degrees * PI / 180;
 }
 
-TEST_STMT("to_radians_0", close_enough(to_radians(0), 0.0f));
-TEST_STMT("to_radians_1", close_enough(to_radians(1), 0.01745329f));
-TEST_STMT("to_radians_2", close_enough(to_radians(-1), -0.01745329f));
-TEST_STMT("to_radians_3", close_enough(to_radians(180), PI));
+TEST_STMT("to_radians", close_enough(to_radians(0), 0.0f));
+TEST_STMT("to_radians", close_enough(to_radians(1), 0.01745329f));
+TEST_STMT("to_radians", close_enough(to_radians(-1), -0.01745329f));
+TEST_STMT("to_radians", close_enough(to_radians(180), PI));
 
 real to_degrees(real radians) {
     return radians * 180 / PI;
 }
 
-TEST_STMT("to_radians_0", close_enough(to_degrees(0), 0.0f));
-TEST_STMT("to_radians_1", close_enough(to_degrees(0.01745329), 1.0f));
-TEST_STMT("to_radians_2", close_enough(to_degrees(-0.01745329), -1.0f));
-TEST_STMT("to_radians_3", close_enough(to_degrees(PI), 180.0f));
-TEST_STMT("to_radians_4", close_enough(to_radians(to_degrees(123)), 123.0f));
+TEST_STMT("to_radians", close_enough(to_degrees(0), 0.0f));
+TEST_STMT("to_radians", close_enough(to_degrees(0.01745329), 1.0f));
+TEST_STMT("to_radians", close_enough(to_degrees(-0.01745329), -1.0f));
+TEST_STMT("to_radians", close_enough(to_degrees(PI), 180.0f));
+TEST_STMT("to_radians", close_enough(to_radians(to_degrees(123)), 123.0f));
 
 } // namespace Math
