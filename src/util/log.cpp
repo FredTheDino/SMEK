68b0fdd628fdce2ad1f4fc1c79e05518e1576b12
Edvard Thörnros <edvard.thornros@gmail.com>
2020-05-18 23:13:50 +0200

    Add ASSERT, CHECK, WARN and UNREACHABLE

--- src/util/log.cpp
+++ src/util/log.cpp
@@ -1,15 +1,27 @@
 #include "log.h"
+#include "color.h"
 #include <cstdio>
+#include <cstdlib>
 #include <cstdarg>
 
 void _smek_error_log(const char *file, u32 line, const char *message, ...) {
-    std::fprintf(stderr, YELLOW "!%s" RESET "|" BLUE "%d" RESET ": ", file, line);
+    std::fprintf(stderr, RED "!%s" RESET "|" BLUE "%d" RESET ": ", file, line);
     va_list args;
     va_start(args, message);
     std::vfprintf(stderr, message, args);
     va_end(args);
     std::fprintf(stderr, "\n");
 }
+
+void _smek_warn_log(const char *file, u32 line, const char *message, ...) {
+    std::fprintf(stderr, YELLOW "?%s" RESET "|" BLUE "%d" RESET ": ", file, line);
+    va_list args;
+    va_start(args, message);
+    std::vfprintf(stderr, message, args);
+    va_end(args);
+    std::fprintf(stderr, "\n");
+}
+
 
 void _smek_info_log(const char *file, u32 line, const char *message, ...) {
     std::fprintf(stderr, GREEN " %s" RESET "|" BLUE "%d" RESET ": ", file, line);
@@ -20,4 +32,27 @@ void _smek_info_log(const char *file, u32 line, const char *message, ...) {
     std::fprintf(stderr, "\n");
 }
 
+void _smek_unreachable(const char *file, u32 line) {
+    std::fprintf(stderr, RED "%s" RESET "|" RED "%d" RESET ": Unreachable\n", file, line);
+    std::fprintf(stderr, BOLDRED "| " RESET " End of Transmission" RESET "\n");
 
+    std::exit('H'); // U for Unreachable
+}
+
+void _smek_assert(const char *file, u32 line, bool passed, const char *msg, const char *expr) {
+    if (passed) return;
+
+    std::fprintf(stderr, RED "%s" RESET "|" RED "%d" RESET ": ASSERT(%s)\n", file, line, expr);
+    std::fprintf(stderr, BOLDRED "| " RESET "%s\n", msg);
+    std::fprintf(stderr, BOLDRED "| " RESET "End of Transmission" RESET "\n");
+
+    std::exit('A'); // A for Assert
+}
+
+bool _smek_check(const char *file, u32 line, bool passed, const char *msg, const char *expr) {
+    if (!passed) {
+        std::fprintf(stderr, YELLOW "%s" RESET "|" BLUE "%d" RESET ": %s\n", file, line, expr);
+        std::fprintf(stderr, YELLOW "| " RESET "%s\n", msg);
+    }
+    return passed;
+}
--- src/util/log.h
+++ src/util/log.h
@@ -1,9 +1,20 @@
 #pragma once
 #include "../math/types.h"
-#include "color.h"
 
 #define ERROR(...) _smek_error_log(__FILE__, __LINE__, __VA_ARGS__)
 void _smek_error_log(const char *file, u32 line, const char *message, ...);
 
+#define WARN(...) _smek_warn_log(__FILE__, __LINE__, __VA_ARGS__)
+void _smek_warn_log(const char *file, u32 line, const char *message, ...);
+
 #define LOG(...) _smek_info_log(__FILE__, __LINE__, __VA_ARGS__)
 void _smek_info_log(const char *file, u32 line, const char *message, ...);
+
+
+#define UNREACHABLE _smek_unreachable(__FILE__, __LINE__)
+void _smek_unreachable(const char *file, u32 line);
+#define STR(x) #x
+#define ASSERT(pass, msg) _smek_assert(__FILE__, __LINE__, pass, msg, STR(pass))
+void _smek_assert(const char *file, u32 line, bool passed, const char *msg, const char *expr);
+#define CHECK(pass, msg) _smek_check(__FILE__, __LINE__, pass, msg, STR(pass))
+bool _smek_check(const char *file, u32 line, bool passed, const char *msg, const char *expr);
