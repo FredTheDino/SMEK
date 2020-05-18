8a4dc72e2a491c5d5884be27a3d339fdb20ac9df
Gustav Sörnäs <gustav@sornas.net>
2020-05-18 23:43:13 +0200

    Add (potentially too verbose) debug output to Asset::load

--- src/asset/asset.cpp
+++ src/asset/asset.cpp
@@ -31,66 +31,65 @@ size_t read(FILE *file, void *ptr, size_t num=1) {
 }
 
 void load(const char *path) {
+    LOG("Reading asset file %s", path);
     FILE *file = fopen(path, "rb");
     if (!file) return;
 
-    LOG("%ld %#lx", ftell(file), ftell(file));
+    LOG("Reading file header");
     read<FileHeader>(file, &system.file_header, 1);
-    LOG("read<FileHeader>: %ld %#lx", ftell(file), ftell(file));
     u64 num_assets = system.file_header.num_assets;
     system.num_assets = num_assets;
 
+    LOG("Reading %d asset headers", num_assets);
     system.headers = new AssetHeader[num_assets];
     read<AssetHeader>(file, system.headers, num_assets);
-    LOG("read<AssetHeader>: %ld %#lx", ftell(file), ftell(file));
 
-#ifdef VERBOSE
     for (u64 asset = 0; asset < num_assets; asset++) {
-        printf("%u %20lu %20lu %lu %#lx\n",
-                system.headers[asset].type,
-                system.headers[asset].name_hash,
-                system.headers[asset].data_hash,
-                system.headers[asset].data_size,
-                system.headers[asset].data_offset);
+        LOG("Found asset with type %u, name %020lu, data %020lu",
+            system.headers[asset].type,
+            system.headers[asset].name_hash,
+            system.headers[asset].data_hash);
     }
-#endif
 
     system.data = new AssetData[num_assets];
     for (u64 asset = 0; asset < num_assets; asset++) {
         AssetHeader header = system.headers[asset];
         AssetData *data_ptr = &system.data[asset];
         fseek(file, system.file_header.data_offset + header.data_offset, SEEK_SET);
-        LOG("fseek: %ld %#lx", ftell(file), ftell(file));
         switch (header.type) {
         case AssetType::TEXTURE: {
+            LOG("Reading Texture at %#lx", ftell(file));
             read<Image>(file, data_ptr);
-            LOG("read<Image>: %ld %#lx", ftell(file), ftell(file));
             u64 size = data_ptr->image.size();
             data_ptr->image.data = new u8[size];
+            LOG("Reading %ld bytes (%dx%dx%d)",
+                size,
+                data_ptr->image.height,
+                data_ptr->image.width,
+                data_ptr->image.channels);
             read<u8>(file, data_ptr->image.data, size);
-            LOG("read<u8>: %ld %#lx", ftell(file), ftell(file));
 
             delete[] data_ptr->image.data;
         } break;
         case AssetType::STRING: {
+            LOG("Reading String at %#lx", ftell(file));
             read<StringAsset>(file, data_ptr);
-            LOG("read<StringAsset>: %ld %#lx", ftell(file), ftell(file));
             u32 size = data_ptr->string.size;
             data_ptr->string.data = new char[size];
+            LOG("Reading %ld bytes", size);
             read<char>(file, data_ptr->string.data, size);
-            LOG("read<char>: %ld %#lx", ftell(file), ftell(file));
 
             delete[] data_ptr->string.data;
         } break;
         case AssetType::MODEL: {
+            LOG("Reading Model at %#lx", ftell(file));
             read<Model>(file, data_ptr);
-            LOG("read<Model>: %ld %#lx", ftell(file), ftell(file));
             u32 points_per_face = data_ptr->model.points_per_face;
             u32 num_faces = data_ptr->model.num_faces;
             u32 size = 8 * points_per_face * num_faces;  // 3+2+3 values per point
             data_ptr->model.data = new f32[size];
+            LOG("Reading %ld bytes (%d faces)", size, num_faces);
             read<f32>(file, data_ptr->model.data, size);
-            LOG("read<f32>: %ld %#lx", ftell(file), ftell(file));
             f32 *data = data_ptr->model.data;
 
             for (u32 i = 0; i < size; i += 8*3) {
@@ -109,6 +108,7 @@ void load(const char *path) {
             delete[] data_ptr->model.data;
         } break;
         default:
+            ERROR("Unknown asset type %d for id %d in asset file %s", header.type, asset, path);
             break;
         }
     }
