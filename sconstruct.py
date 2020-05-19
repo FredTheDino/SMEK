8c2bc53af58e0b7f8d20b847a2bfec1353d4cb05
Gustav Sörnäs <gustav@sornas.net>
2020-05-20 00:58:44 +0200

    Filter all output through c++filt if it exists in path

--- sconstruct.py
+++ sconstruct.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import re
 from glob import glob
 from subprocess import Popen, PIPE
@@ -51,7 +52,16 @@ smek = env.Program(target="bin/SMEK", source=source)
 Depends(smek, assets)
 Default(smek)
 
-AlwaysBuild(env.Alias("run", smek, smek[0].abspath))
+run_cmd = smek[0].abspath
+if shutil.which("c++filt"):
+    # Swap stdout and stderr, de-mangle with c++filt, then swap back.
+    # https://serverfault.com/questions/63705/how-to-pipe-stderr-without-piping-stdout
+    run_cmd += " 3>&1 1>&2 2>&3 | c++filt 3>&2 2>&1 1>&3 | c++filt"
+else:
+    # c++filt should be installed but just in case
+    run_cmd += "&& echo \"c++filt not found, function names may be mangled\""
+
+AlwaysBuild(env.Alias("run", smek, run_cmd))
 
 docs = env.Alias("docs", "", "docs/doc-builder.py")
 AlwaysBuild(docs)
