98e68caf3b90f02d1b7155e8e295b699be8c6803
Edvard Thörnros <edvard.thornros@gmail.com>
2020-05-23 23:31:13 +0200

    Add input tests

--- src/input.cpp
+++ src/input.cpp
@@ -1,5 +1,6 @@
 #include "input.h"
 #include "game.h"
+#include "test.h"
 
 namespace Input {
 
@@ -30,5 +31,55 @@ bool pressed(Action name) {
 f32 value(Action name) {
     return get_current(name);
 }
+
+#define SOME_ACTION ((Ac) 0)
+
+TEST_CASE("input_down", {
+    f32 *current = game->input.current_frame;
+    f32 *last = game->input.last_frame;
+    *current = 0.0;
+    *last = 0.0;
+    bool success = true;
+    success |= down(SOME_ACTION);
+    *last = 1.0;
+    success |= down(SOME_ACTION);
+    return success;
+});
+
+TEST_CASE("input_up", {
+    f32 *current = game->input.current_frame;
+    f32 *last = game->input.last_frame;
+    *current = 0.0;
+    *last = 0.0;
+    bool success = true;
+    success |= up(SOME_ACTION);
+    *last = 1.0;
+    success |= up(SOME_ACTION);
+    return success;
+});
+
+TEST_CASE("input_released", {
+    f32 *current = game->input.current_frame;
+    f32 *last = game->input.last_frame;
+    *current = 0.0;
+    *last = 0.0;
+    bool success = true;
+    success |= !released(SOME_ACTION);
+    *last = 1.0;
+    success |= released(SOME_ACTION);
+    return true;
+});
+
+TEST_CASE("input_pressed", {
+    f32 *current = game->input.current_frame;
+    f32 *last = game->input.last_frame;
+    *current = 1.0;
+    *last = 0.0;
+    bool success = true;
+    success |= pressed(SOME_ACTION);
+    *last = 1.0;
+    success |= !pressed(SOME_ACTION);
+    return true;
+});
 
 }
