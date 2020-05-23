bd5b028f335122c66fd922abbad689d5e8515981
Edvard Thörnros <edvard.thornros@gmail.com>
2020-05-24 00:11:33 +0200

    Add tests for rebindable keys

--- src/platform.cpp
+++ src/platform.cpp
@@ -220,19 +220,19 @@ int main() { // Game entrypoint
     input.bind(SOME_ACTION, 1, button_2, -1.0)
 
 #include "test.h"
-TEST_CASE("platform_input", {
+TEST_CASE("platform_input_press", {
     DEFAULT_INPUT_TEST;
     input.update_press(button_3, true);
     return input.state[0] == 0.0;
 });
 
-TEST_CASE("platform_input", {
+TEST_CASE("platform_input_press", {
     DEFAULT_INPUT_TEST;
     input.update_press(button_1, true);
     return input.state[0] == 1.0;
 });
 
-TEST_CASE("platform_input", {
+TEST_CASE("platform_input_press", {
     DEFAULT_INPUT_TEST;
     input.update_press(button_1, true);
     input.update_press(button_1, false);
@@ -241,7 +241,7 @@ TEST_CASE("platform_input", {
     return input.state[0] == 1.0;
 });
 
-TEST_CASE("platform_input", {
+TEST_CASE("platform_input_press", {
     DEFAULT_INPUT_TEST;
     input.update_press(button_3, true);
     input.update_press(button_2, true);
@@ -249,10 +249,51 @@ TEST_CASE("platform_input", {
     return input.state[0] == -1.0;
 });
 
-TEST_CASE("platform_input", {
+TEST_CASE("platform_input_press", {
     DEFAULT_INPUT_TEST;
     input.update_press(button_1, false);
     input.update_press(button_1, false);
+    input.update_press(button_1, false);
+    return input.state[0] == 0.0;
+});
+
+TEST_CASE("platform_input_rebind", {
+    DEFAULT_INPUT_TEST;
+    input.update_press(button_1, true);
+    input.update_press(button_1, false);
+    input.unbind(SOME_ACTION, 0);
+    input.update_press(button_1, true);
+    input.update_press(button_1, false);
+    return input.state[0] == 0.0;
+});
+
+TEST_CASE("platform_input_rebind", {
+    DEFAULT_INPUT_TEST;
+    input.update_press(button_3, true);
+    input.update_press(button_3, false);
+    input.unbind(SOME_ACTION, 0);
+    input.bind(SOME_ACTION, 0, button_3);
+    input.update_press(button_1, true);
+    input.update_press(button_1, false);
+    return input.state[0] == 0.0;
+});
+
+TEST_CASE("platform_input_rebind", {
+    DEFAULT_INPUT_TEST;
+    input.update_press(button_3, true);
+    input.update_press(button_3, false);
+    input.unbind(SOME_ACTION, 0);
+    input.bind(SOME_ACTION, 0, button_3);
+    input.update_press(button_3, true);
+    return input.state[0] == 1.0;
+});
+
+TEST_CASE("platform_input_rebind", {
+    DEFAULT_INPUT_TEST;
+    input.update_press(button_1, true);
+    input.unbind(SOME_ACTION, 0);
+    input.update_press(button_1, false);
+    input.bind(SOME_ACTION, 0, button_1);
     input.update_press(button_1, false);
     return input.state[0] == 0.0;
 });
