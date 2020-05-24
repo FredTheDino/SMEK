f6f430d3e09985b431fbef98377202197ada8b91
Edvard Thörnros <edvard.thornros@gmail.com>
2020-05-24 22:28:31 +0200

    Move to using GLAD

--- sconstruct.py
+++ sconstruct.py
@@ -39,6 +39,7 @@ env.Append(CXXFLAGS="-ggdb")
 env.Append(CXXFLAGS="-O0")
 env.Append(CXXFLAGS="-Wno-unused")
 env.Append(CXXFLAGS=shell(["sdl2-config", "--cflags"]))
+env.Append(CXXFLAGS="-Iinc")
 env.Append(LINKFLAGS=shell(["sdl2-config", "--libs"]))
 env.Append(LINKFLAGS="-ldl")
 env.Append(LINKFLAGS="-rdynamic")  # Gives backtrace information
--- src/game.cpp
+++ src/game.cpp
@@ -43,8 +43,8 @@ void reload_game(GameState *game) {
 }
 
 GameState update_game(GameState *game, GSUM mode) { // Game entry point
-    GL::ClearColor(0.2, 0.1, 0.3, 1); // We don't need to do this...
-    GL::Clear(GL::cCOLOR_BUFFER_BIT | GL::cDEPTH_BUFFER_BIT);
+    glClearColor(0.2, 0.1, 0.3, 1); // We don't need to do this...
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     if (Input::pressed(Ac::Rebind)) {
         Input::rebind(Ac::AButton);
@@ -52,14 +52,14 @@ GameState update_game(GameState *game, GSUM mode) { // Game entry point
 
     real time = SDL_GetTicks() / 1000.0;
 
-    auto t_loc = GL::GetUniformLocation(shader.program_id, "t");
-    GL::Uniform1f(t_loc, time);
+    auto t_loc = glGetUniformLocation(shader.program_id, "t");
+    glUniform1f(t_loc, time);
 
-    auto proj_loc = GL::GetUniformLocation(shader.program_id, "proj");
+    auto proj_loc = glGetUniformLocation(shader.program_id, "proj");
     Mat proj_matrix = Mat::perspective(PI / 3, 0.01, 3.0);
-    GL::UniformMatrix4fv(proj_loc, 1, true, proj_matrix.data());
+    glUniformMatrix4fv(proj_loc, 1, true, proj_matrix.data());
 
-    auto view_loc = GL::GetUniformLocation(shader.program_id, "view");
+    auto view_loc = glGetUniformLocation(shader.program_id, "view");
 
     static f32 x = 0;
 
@@ -73,29 +73,29 @@ GameState update_game(GameState *game, GSUM mode) { // Game entry point
     Vec3 target = Vec3(Math::cos(time) * 0.2, Math::sin(time) * 0.0, -0.5);
     Vec3 up = Vec3(0, 1, 0);
     Mat view_matrix = Mat::look_at(from, target, up);
-    GL::UniformMatrix4fv(view_loc, 1, true, view_matrix.data());
+    glUniformMatrix4fv(view_loc, 1, true, view_matrix.data());
 
 
     shader.use();
 
-    auto tex_loc = GL::GetUniformLocation(shader.program_id, "tex");
+    auto tex_loc = glGetUniformLocation(shader.program_id, "tex");
     texture.bind(0);
-    GL::Uniform1i(tex_loc, 0);
+    glUniform1i(tex_loc, 0);
 
-    auto model_loc = GL::GetUniformLocation(shader.program_id, "model");
+    auto model_loc = glGetUniformLocation(shader.program_id, "model");
     Mat model_matrix = Mat::translate(Math::cos(time) * 0.2, Math::sin(time) * 0.2, -0.5) * Mat::scale(0.1);
-    GL::UniformMatrix4fv(model_loc, 1, true, model_matrix.data());
+    glUniformMatrix4fv(model_loc, 1, true, model_matrix.data());
     mesh.draw();
 
 #if 0
     model_matrix = Mat::translate(-Math::cos(time) * 0.2, -Math::sin(time) * 0.2, -0.5) * Mat::scale(0.1);
-    GL::UniformMatrix4fv(model_loc, 1, true, model_matrix.data());
+    glUniformMatrix4fv(model_loc, 1, true, model_matrix.data());
     mesh.draw();
 #endif
 
-    model_loc = GL::GetUniformLocation(shader.program_id, "model");
+    model_loc = glGetUniformLocation(shader.program_id, "model");
     model_matrix = Mat::translate(0, 0, -0.6) * Mat::scale(0.1);
-    GL::UniformMatrix4fv(model_loc, 1, true, model_matrix.data());
+    glUniformMatrix4fv(model_loc, 1, true, model_matrix.data());
     mesh.draw();
 
 
