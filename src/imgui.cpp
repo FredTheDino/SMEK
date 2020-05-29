396e46527470e1fa31c1446c32cc32f2231aa1f3
Edvard Thörnros <edvard.thornros@gmail.com>
2020-05-29 23:33:32 +0200

    Move IMGUI

---
+++ src/imgui.cpp
@@ -0,0 +1,8 @@
+// Compilation unit for IMGUI
+
+#include <imgui/imgui.cpp>
+#include <imgui/imgui_demo.cpp>
+#include <imgui/imgui_draw.cpp>
+#include <imgui/imgui_impl_opengl3.cpp>
+#include <imgui/imgui_impl_sdl.cpp>
+#include <imgui/imgui_widgets.cpp>
