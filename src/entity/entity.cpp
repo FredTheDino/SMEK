c700db5dabe82975b0078dfb08c6b41c61301775
Edvard Thörnros <edvard.thornros@gmail.com>
2020-06-04 23:05:13 +0200

    Add basic movement and camera control

--- src/entity/entity.cpp
+++ src/entity/entity.cpp
@@ -4,7 +4,37 @@
 #include "../game.h"
 
 void Player::update() {
-    position = Vec3(Math::cos(time()) * 0.2, Math::sin(time()) * 0.2, -0.5);
+    const f32 floor = 0.2;
+    const f32 jump_speed = 2.0;
+    const f32 movement_speed = 4.0;
+
+    Vec2 turn = Input::mouse_move();
+    turn = turn * delta();
+    rotation = normalized(H::from(0.0, -turn.x, 0.0) * rotation * H::from(-turn.y, 0.0, 0.0));
+
+    f32 drag_coef = Math::pow(0.05, delta());
+    velocity.x = velocity.x * drag_coef;
+    velocity.y -= 4.82 * delta(); // Temporary gravity
+    velocity.z = velocity.z * drag_coef;
+    velocity += rotation * Vec3(Input::value(Ac::MoveX), 0.0, Input::value(Ac::MoveZ)) * movement_speed * delta();
+    // Plane collision
+    if (position.y <= floor) {
+        position.y = floor;
+        velocity.y = 0.0;
+
+        // If grounded
+        if (Input::pressed(Ac::Jump) && velocity.y == 0.0) {
+            velocity.y = jump_speed;
+        }
+    }
+
+    if (Input::pressed(Ac::Shoot)) {
+        LOG("Pew!");
+    }
+    position += velocity * delta();
+
+    GFX::main_camera()->position = position + Vec3(0.0, 0.3, 0.0);
+    GFX::main_camera()->rotation = rotation;
 }
 
 void Player::draw() {
