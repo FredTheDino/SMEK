a81cd5f1e98dde115e2a30bfaf69b221688aa42b
Gustav Sörnäs <gustav@sornas.net>
2020-06-08 14:45:49 +0200

    Rename callback macro in handle_events()

--- src/event.cpp
+++ src/event.cpp
@@ -5,16 +5,16 @@
 namespace EventSystem {
 
 void handle_events() {
-#define CASE(NAME) case NAME: e.NAME.callback(); break
+#define HANDLE(NAME) case NAME: e.NAME.callback(); break
     GameState *state = GAMESTATE();
     Event e;
     while (!state->event_queue.empty()) {
         e = state->event_queue.front();
         state->event_queue.pop();
         switch (e.type) {
-        CASE(CREATE_SOUND_ENTITY);
-        default:
-            ERROR("Unkown event type {}", e.type);
+            HANDLE(CREATE_SOUND_ENTITY);
+            default:
+                ERROR("Unkown event type {}", e.type);
         }
     }
 }
