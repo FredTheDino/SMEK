eccda418f508e54f394221ff01988d2b09dd4126
Gustav Sörnäs <gustav@sornas.net>
2020-06-18 09:15:34 +0200

    Use window and gl context from game state

--- src/platform.cpp
+++ src/platform.cpp
@@ -199,11 +199,9 @@ static void imgui_end_frame() {
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-    SDL_Window *backup_current_window = SDL_GL_GetCurrentWindow();
-    SDL_GLContext backup_current_context = SDL_GL_GetCurrentContext();
     ImGui::UpdatePlatformWindows();
     ImGui::RenderPlatformWindowsDefault();
-    SDL_GL_MakeCurrent(backup_current_window, backup_current_context);
+    SDL_GL_MakeCurrent(game_state.window, game_state.gl_context);
 }
 #else
 static void imgui_platform_start() {}
