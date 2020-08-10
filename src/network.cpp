7673b44dacc8a05e7993c6503235686900ee0596
Gustav Sörnäs <gustav@sornas.net>
2020-08-10 23:53:09 +0200

    Reorder to match header

--- src/network.cpp
+++ src/network.cpp
@@ -65,50 +65,6 @@ int start_network_handle(void *data) {
     return 0;
 }
 
-bool Network::connect_to_server(char *hostname, int portno) {
-    if (server_handle.active) {
-        WARN("Already connected to server");
-        return false;
-    }
-    server_handle.sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_handle.sockfd < 0) {
-        ERR("Error opening socket");
-        return false;
-    }
-    hostent *server = gethostbyname(hostname);
-    if (!server) {
-        ERR("No such host");
-        return false;
-    }
-    sockaddr_in serv_addr = {};
-    serv_addr.sin_family = AF_INET;
-    std::memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (::connect(server_handle.sockfd, (sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        WARN("Unable to connect to server");
-        return false;
-    }
-    LOG("Connected");
-    server_handle.active = true;
-    server_handle.thread = SDL_CreateThread(start_network_handle, "ServerHandle", (void *) &server_handle);
-    if (!server_handle.thread) {
-        ERR("Unable to create thread");
-        return false;
-    }
-    return true;
-}
-
-void Network::disconnect_from_server() {
-    if (!server_handle.active) {
-        WARN("Not connected to a server");
-        return;
-    }
-    server_handle.active = false;
-    shutdown(server_handle.sockfd, SHUT_RDWR);
-    close(server_handle.sockfd);
-    SDL_WaitThread(server_handle.thread, NULL);
-}
-
 bool Network::setup_server(int portno) {
     if (server_listening) {
         WARN("Server already running");
@@ -152,6 +108,50 @@ void Network::stop_server() {
         }
     }
     LOG("Server stopped");
+}
+
+bool Network::connect_to_server(char *hostname, int portno) {
+    if (server_handle.active) {
+        WARN("Already connected to server");
+        return false;
+    }
+    server_handle.sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_handle.sockfd < 0) {
+        ERR("Error opening socket");
+        return false;
+    }
+    hostent *server = gethostbyname(hostname);
+    if (!server) {
+        ERR("No such host");
+        return false;
+    }
+    sockaddr_in serv_addr = {};
+    serv_addr.sin_family = AF_INET;
+    std::memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (::connect(server_handle.sockfd, (sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        WARN("Unable to connect to server");
+        return false;
+    }
+    LOG("Connected");
+    server_handle.active = true;
+    server_handle.thread = SDL_CreateThread(start_network_handle, "ServerHandle", (void *) &server_handle);
+    if (!server_handle.thread) {
+        ERR("Unable to create thread");
+        return false;
+    }
+    return true;
+}
+
+void Network::disconnect_from_server() {
+    if (!server_handle.active) {
+        WARN("Not connected to a server");
+        return;
+    }
+    server_handle.active = false;
+    shutdown(server_handle.sockfd, SHUT_RDWR);
+    close(server_handle.sockfd);
+    SDL_WaitThread(server_handle.thread, NULL);
 }
 
 bool Network::new_client_handle(int newsockfd) {
