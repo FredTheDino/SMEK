dfb239e242979417eee71167d5e60a0996846658
Gustav Sörnäs <gustav@sornas.net>
2020-08-27 23:25:04 +0200

    Remove more code for PackageA and PackageB

--- src/network/package.cpp
+++ src/network/package.cpp
@@ -27,13 +27,6 @@ Package unpack(u8 *from) {
 
 i32 format(char *buffer, u32 size, FormatHint args, Package pkg) {
     switch (pkg.header.type) {
-    case PackageType::A:
-        return snprintf(buffer, size, "A: a=%0*d",
-                        args.num_zero_pad, pkg.A.a);
-    case PackageType::B:
-        return snprintf(buffer, size, "B: a=%0*d, b=%0*d",
-                        args.num_zero_pad, pkg.B.a,
-                        args.num_zero_pad, pkg.B.b);
     case PackageType::SET_CLIENT_ID:
         return snprintf(buffer, size, "SetClientID: id=%0*u",
                         args.num_zero_pad, pkg.SET_CLIENT_ID.client_id);
@@ -60,13 +53,6 @@ void imgui_package_create(Package *package, WipEntities *wip_entities) {
     ImGui::Combo("Package type", &pkg_type_current_id, package_type_list, LEN(package_type_list));
     package->header.type = (PackageType)pkg_type_current_id;
     switch (package->header.type) {
-    case PackageType::A:
-        ImGui::InputInt("a", &package->A.a);
-        break;
-    case PackageType::B:
-        ImGui::InputInt("a", &package->B.a);
-        ImGui::InputInt("b", &package->B.b);
-        break;
     case PackageType::EVENT:
         imgui_event_create(&package->EVENT.event, wip_entities);
         break;
@@ -107,12 +93,6 @@ void imgui_event_create(Event *event, WipEntities *wip_entities) {
 
 void imgui_package_show(Package *package) {
     switch (package->header.type) {
-    case PackageType::A:
-        ImGui::Text("A\n a=%d", package->A.a);
-        break;
-    case PackageType::B:
-        ImGui::Text("B\n a=%d\n b=%d", package->B.a, package->B.b);
-        break;
     default:
         ImGui::Text("Not implemented for package type %u", (u32)package->header.type);
         break;
