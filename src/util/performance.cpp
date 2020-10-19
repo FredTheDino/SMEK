e12f5b16305e20a24ae111261349467a8d4b05a2
Edvard Thörnros <edvard.thornros@gmail.com>
2020-10-19 23:10:55 +0200

    Add frametime and average frametime

--- src/util/performance.cpp
+++ src/util/performance.cpp
@@ -13,6 +13,10 @@ namespace Performance {
 struct PerformanceMetrics {
     // TODO(ed): Locks and working with different threads.
     std::unordered_map<u64, PerformanceCounter> metrics;
+
+    TimePoint frame_start = {};
+    f32 frame_time[HISTORY_LENGTH] = {};
+    f32 average_frame_time[HISTORY_LENGTH] = {};
     u32 frame = 0;
 } gpc;
 
@@ -62,9 +66,41 @@ void report() {
         f32 ys[] = { -10, 30 };                        \
         ImPlot::PlotLine("NOW", xs, ys, 2);            \
     } while (false)
+
+    using std::chrono::duration_cast;
+    using std::chrono::high_resolution_clock;
+    using std::chrono::nanoseconds;
+    TimePoint now = high_resolution_clock::now();
+    f32 frame_time = duration_cast<nanoseconds>(now - gpc.frame_start).count() * NANO_TO_MS;
+    frame_time = frame_time < 40 ? frame_time : 40;
+    gpc.frame_start = now;
+
+    u32 prev_frame = gpc.frame;
     gpc.frame += 1;
     gpc.frame %= HISTORY_LENGTH;
     ImPlot::SetNextPlotLimits(0, HISTORY_LENGTH, 0, 16);
+
+    ImPlot::SetNextPlotLimits(0, HISTORY_LENGTH, 0, 20);
+    if (ImPlot::BeginPlot("Total Frame Time",
+                          "Frame",
+                          "Time (ms)",
+                          Vec2(-1, 0),
+                          ImPlotFlags_None,
+                          ImPlotAxisFlags_Lock | ImPlotAxisFlags_NoDecorations,
+                          ImPlotAxisFlags_Lock)) {
+
+        DRAW_NOW_LINE;
+        f32 prev_average = gpc.average_frame_time[prev_frame];
+        f32 oldest_frame_time = gpc.frame_time[gpc.frame];
+        f32 delta_frame_time = frame_time - oldest_frame_time;
+        f32 new_average = prev_average + delta_frame_time / HISTORY_LENGTH;
+        gpc.average_frame_time[gpc.frame] = new_average;
+        gpc.frame_time[gpc.frame] = frame_time;
+        ImPlot::PlotLine("Raw Frametime", gpc.frame_time, HISTORY_LENGTH);
+        ImPlot::PlotLine("Average Frametime", gpc.average_frame_time, HISTORY_LENGTH);
+        ImPlot::EndPlot();
+    }
+
     if (ImPlot::BeginPlot("Total Time",
                           "Frame",
                           "Time (ms)",
