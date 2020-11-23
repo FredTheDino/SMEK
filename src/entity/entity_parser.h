8046c9e7ede07b6bd5b69831365f4bdc78446499
Edvard Thörnros <edvard.thornros@gmail.com>
2020-11-23 19:21:10 +0100

    Fix cliff-hanger

--- src/entity/entity_parser.h
+++ src/entity/entity_parser.h
@@ -7,7 +7,8 @@ using EntityParseCallback = std::function<void(BaseEntity *)>;
 
 ///*
 // Reads text and converts it to entities, the lifetime of the
-// BaseEntity pointer is
+// BaseEntity pointer given to the EntityParseCallback is only
+// valid for that call.
 void parse_entities_and_do(const char *data, const char *filename, EntityParseCallback);
 
 // TODO(ed): Add a way to serialize entities.
